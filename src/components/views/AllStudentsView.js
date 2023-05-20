7143374443403a26e30ecd1afda4a72a1511924c
Jiahao <93109917+Jiahao0804@users.noreply.github.com>
2023-05-20 04:43:23 -0400

    Modify AllStudentView

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -5,6 +5,7 @@ The Views component is responsible for rendering web page with data provided by 
 It constructs a React component to display the all students view page.
 ================================================== */
 import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
 
 const AllStudentsView = (props) => {
   const {students, deleteStudent} = props;
